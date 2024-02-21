b76bd946e7912358d9bf8b9518339ba56f5f3bdd
Rahul <rahuljha@29052002@gmail.com>
2024-02-21 20:22:07 +0530

    Random paths and intervals for the enemy to follow

--- script.js
+++ script.js
@@ -3,35 +3,38 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 40;
+const numberOfEnemies = 20;
 
 const enemyImg = new Image();
-enemyImg.src = "img/enemy3.png";
+enemyImg.src = "img/enemy4.png";
 
 let gameFrame=0;
 class Enemy {
     constructor(image) {
         this.image = image;
         this.speed = Math.random() * 4;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.x = Math.random() * (CANVAS_WIDTH - this.width);
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 2 + 0.5;
-        this.curve = Math.random() * 200 + 50;
+        this.newx = Math.random() * (CANVAS_WIDTH - this.width);
+        this.newy = Math.random() * (CANVAS_HEIGHT - this.height);
+        this.interval = Math.floor(Math.random() * 200 + 50);
     }
     
     update() {
-        this.x=canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-        this.y=canvas.height/2 * Math.cos(this.angle * Math.PI/90) + (canvas.height/2 - this.height/2);
-        // if(this.x + this.width < 0) this.x = CANVAS_WIDTH;
-        // this.y+=this.curve * Math.sin(this.angle);
-        this.angle+=this.angleSpeed;
+        if(gameFrame % this.interval === 0) {
+            this.newx = Math.random() * (CANVAS_WIDTH - this.width);
+            this.newy = Math.random() * (CANVAS_HEIGHT - this.height);
+        }
+        let dx = this.x - this.newx;
+        let dy = this.y - this.newy;
+        this.x-=dx/20;
+        this.y-=dy/20;
         if(gameFrame % this.flapSpeed === 0) {
             this.frame+=1;
             this.frame%=4;
