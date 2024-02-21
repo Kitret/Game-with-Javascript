f305e7a34b735121ac0a9ed2c3d4132a29a989a0
Rahul <rahuljha@29052002@gmail.com>
2024-02-21 20:15:32 +0530

    Using boths sin and cos function for unique movenments of enemey characters

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 40;
 
 const enemyImg = new Image();
-enemyImg.src = "img/enemy2.png";
+enemyImg.src = "img/enemy3.png";
 
 let gameFrame=0;
 class Enemy {
     constructor(image) {
         this.image = image;
         this.speed = Math.random() * 4;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.frame = 0;
@@ -22,14 +22,15 @@ class Enemy {
         this.x = Math.random() * (CANVAS_WIDTH - this.width);
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2 + 0.02;
-        this.curve = Math.random() * 7;
+        this.angleSpeed = Math.random() * 2 + 0.5;
+        this.curve = Math.random() * 200 + 50;
     }
     
     update() {
-        this.x-=this.speed;
-        if(this.x + this.width < 0) this.x = CANVAS_WIDTH;
-        this.y+=this.curve * Math.sin(this.angle);
+        this.x=canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
+        this.y=canvas.height/2 * Math.cos(this.angle * Math.PI/90) + (canvas.height/2 - this.height/2);
+        // if(this.x + this.width < 0) this.x = CANVAS_WIDTH;
+        // this.y+=this.curve * Math.sin(this.angle);
         this.angle+=this.angleSpeed;
         if(gameFrame % this.flapSpeed === 0) {
             this.frame+=1;
