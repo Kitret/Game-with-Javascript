32bca158d5fc2ddbaed2f4cd9e597784ca4e858e
Rahul <rahuljha@29052002@gmail.com>
2024-02-24 18:36:22 +0530

    Showing boom animation on click of mouse on canvas

--- script.js
+++ script.js
@@ -8,26 +8,56 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x,y) {
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x - (this.spriteWidth/2);
+        this.y = y - (this.spriteHeight/2);
         this.image = new Image();
-        this.image.src = 'boom.png';
+        this.image.src = 'img/boom.png';
         this.frame = 0;
-
+        this.timer = 0;
     }
 
     update() {
-        this.frame++;
+        this.timer++;
+        if(this.timer % 10 === 0) {
+            this.frame++;
+        }
     }
 
     draw() {
-        ctx.drawImage(this.image,this.frame * this.spriteWidth, this.spriteHeight,this.x,this.y,this.spriteWidth,this.spriteHeight);
+        ctx.drawImage(this.image,this.frame * this.spriteWidth, 0,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width,this.height);
     }
 }
 
 window.addEventListener('click', (e) => {
-    ctx.fillStyle = "white";
-    ctx.fillRect(e.x - canvasPosition.left - 25,e.y - canvasPosition.top - 25,50,50);
+    createAnimation(e);
 });
+
+// window.addEventListener("mousemove", (e) => {
+//     createAnimation(e);
+// });
+
+function createAnimation(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    console.log(explosions);
+    explosions.push(new Explosion(positionX,positionY));
+}
+
+function animate() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for(let i=0;i<explosions.length;i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        if(explosions[i].frame>5) {
+            explosions.splice(i,1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
