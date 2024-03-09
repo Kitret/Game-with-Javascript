7352ea3acd4312fddee7acb15c436ee093a14243
Rahul <rahuljha@29052002@gmail.com>
2024-03-09 19:00:37 +0530

    Adding mobile swipe update and updating collision detection

--- script.js
+++ script.js
@@ -3,19 +3,34 @@
 document.addEventListener("DOMContentLoaded",(e) => {
 
     const canvas = document.getElementById("canvas1");
+    const fullScreenButton = document.getElementById("toggleFullScreen");
     const ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight*2/3;
+    canvas.width = 1400;
+    canvas.height = 720;
     let enemies=[];
     let score=0;
     let gameOver=false;
 
+    function restartGame(context) {
+        player.restartGame();
+        background.restartGame();
+        enemies=[];
+        score=0;
+        gameOver=false;
+        animate(0);
+    }
+
     class InputHandler {
         constructor() {
             this.keys = [];
+            this.touchY=0;
+            this.touchTreshold=30;
             window.addEventListener("keydown",(e) => {
                 if((e.key === "ArrowDown" || e.key==="ArrowUp" || e.key==="ArrowLeft" || e.key==="ArrowRight") && this.keys.indexOf(e.key)===-1) {
                     this.keys.push(e.key);
+                }
+                else if(e.key === "Enter" && gameOver) {
+                    restartGame();
                 }
             });
 
@@ -24,8 +39,42 @@ document.addEventListener("DOMContentLoaded",(e) => {
                     this.keys.splice(this.keys.indexOf(e.key),1);
                 }
             });
+
+            window.addEventListener("touchstart", (e) => {
+                this.touchY=e.changedTouches[0].pageY;
+            });
+            
+            window.addEventListener("touchmove", (e) => {
+                const swipeDist=e.changedTouches[0].pageY-this.touchY;
+                if(swipeDist<-this.touchTreshold && this.keys.indexOf("swipe up") === -1) {
+                    this.keys.push("swipe up");
+                }
+                if(swipeDist>this.touchTreshold && this.keys.indexOf("swipe down") === -1) {
+                    this.keys.push("swipe down");
+                    if(gameOver) restartGame();
+                }
+                console.log(this.keys);
+            });
+
+            window.addEventListener("touchend", (e) => {
+                this.keys.splice(this.keys.indexOf("splice up"),1);
+                this.keys.splice(this.keys.indexOf("splice down"),1);
+            });
         }
     }
+
+    function toggleFullScreen() {
+        console.log(document.fullscreenElement);
+        if(!document.fullscreenElement) {
+            canvas.requestFullscreen().catch((error) => {
+                alert(`Error, can't enable full screen mode : ${error.message}`);
+            });
+        }
+        else {
+            document.exitFullscreen();
+        }
+    }
+    fullScreenButton.addEventListener("click", toggleFullScreen);
 
     class Player {
         constructor(gameWidth,gameHeight) {
@@ -33,7 +82,7 @@ document.addEventListener("DOMContentLoaded",(e) => {
             this.gameHeight=gameHeight;
             this.width=200;
             this.height=200;
-            this.x=0;
+            this.x=100;
             this.y=this.gameHeight-this.height;
             this.image=document.getElementById("playerImage");
             this.frameX=0;
@@ -41,19 +90,27 @@ document.addEventListener("DOMContentLoaded",(e) => {
             this.speed=0;
             this.vy=0;
             this.weight=1;
-            this.maxFrameX0=9;
+            this.maxFrameX=9;
             this.fps=40;
             this.frameTimer=0;
             this.frameInterval=1000/this.fps;
         }
 
+        restartGame() {
+            this.x=100;
+            this.y=this.gameHeight-this.height;
+            this.frameTimer=0;
+            this.maxFrameX=9;
+            this.frameY=0;
+        }
+
         update(input,deltaTime,enemies) {
             // collision detection
             enemies.forEach((enemy) => {
-                const dx=(enemy.x+enemy.width/2)-(this.x+this.width/2);
-                const dy=(enemy.y+enemy.height/2)-(this.y+this.height/2);
+                const dx=(enemy.x+enemy.width/2-20)-(this.x+this.width/2);
+                const dy=(enemy.y+enemy.height/2)-(this.y+this.height/2+20);
                 const dist=Math.sqrt(dx*dx+dy*dy);
-                if(dist<enemy.width/3+this.width/2) {
+                if(dist<enemy.width/3+this.width/3) {
                     gameOver=true;
                 }
             });
@@ -72,7 +129,7 @@ document.addEventListener("DOMContentLoaded",(e) => {
             else if(input.keys.indexOf("ArrowLeft") > -1) {
                 this.speed-=5;
             }
-            else if(input.keys.indexOf("ArrowUp") > -1 && this.#onGround()) {
+            else if((input.keys.indexOf("ArrowUp") > -1 || input.keys.indexOf("swipe up") > -1) && this.#onGround()) {
                 this.vy-=28;
             }
             else {
@@ -118,6 +175,10 @@ document.addEventListener("DOMContentLoaded",(e) => {
             this.width=2400;
             this.height=720;
             this.speed=7;
+        }
+
+        restartGame() {
+            this.x=0;
         }
 
         update() {
@@ -185,6 +246,7 @@ document.addEventListener("DOMContentLoaded",(e) => {
     }
 
     function displayStatusText(context) {
+        context.textAlign="left";
         context.font="40px Helvetica";
         context.fillStyle="black";
         context.fillText("Score : "+score,20,50);
@@ -193,9 +255,9 @@ document.addEventListener("DOMContentLoaded",(e) => {
         if(gameOver) {
             context.textAlign="center";
             context.fillStyle="black";
-            context.fillText("Game Over! Try Again",canvas.width/2,200);
+            context.fillText("Game Over! Press Enter or Swipe Down to Try Again",canvas.width/2,200);
             context.fillStyle="white";
-            context.fillText("Game Over! Try Again",canvas.width/2+2,200);
+            context.fillText("Game Over! Press Enter or Swipe Down to Try Again",canvas.width/2+2,200);
         }
     }
 
